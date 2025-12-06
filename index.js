6032083c90b39b59a88135cf71383882054ff96e
samsul alam <samsul7718@gmail.com>
2025-12-06 15:58:54 +0530

    product link added succcesfully

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-import express from 'express';;
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import products from './product.js';
+
+
+dotenv.config();
 
 const app=express();
+
+app.use(cors({
+    origin:[
+        // "https://ecommerce-three-umber.vercel.app",
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "http://localhost:5174"
+    ],
+    methods:['GET','POST'],
+    credentials:true
+}))
 
 app.get('/',(req,res)=>{
     res.send("Server is ready to serve");
 })
+
+app.get("/api/product", (req, res) => {
+  res.json(products);
+});
 
 const port=process.env.PORT || 3000;
 
