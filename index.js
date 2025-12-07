2893f2d382b46288e2e8350909ce00f206c1b496
samsul alam <samsul7718@gmail.com>
2025-12-07 22:21:36 +0530

    live server added

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.use(cors({
     origin:[
         // "https://ecommerce-three-umber.vercel.app",
         
-        // "https://hubcredo-frontend-nxxw.vercel.app",
-        "http://localhost:3000",
+        // "http://localhost:3000",
+        "https://hubcredo-frontend-nxxw.vercel.app",
         "http://localhost:5173",
         "http://localhost:5174"
     ],
